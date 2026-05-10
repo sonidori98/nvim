d4e99eff90e5a15c7b1aabbc1cbcce9e022a9e5a
sonidori <sonidori98@proton.me>
2026-05-10 18:16:00 +0900

    ✨ Neo-treeの幅を調整

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -1,11 +1,16 @@
 return {
-	"nvim-neo-tree/neo-tree.nvim",
-	branch = "v3.x",
-	-- lazy = false,
-	keys = {
-		{ "<C-n>", "<cmd>Neotree toggle<cr>", desc = "Neo-tree toggle" },
-	},
-	opts = {
-		close_if_last_window = true,
-	},
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    -- lazy = false,
+    keys = {
+        { "<C-n>", "<cmd>Neotree toggle<cr>", desc = "Neo-tree toggle" },
+    },
+    opts = {
+        close_if_last_window = true,
+        filesystem = {
+            window = {
+                width = 25
+            },
+        },
+    },
 }
