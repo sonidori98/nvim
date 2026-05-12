cf29722442730cc93b95b855f8539f3d50d195f6
sonidori <sonidori98@proton.me>
2026-05-13 02:52:07 +0900

    キーマップを追加

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,2 +1,23 @@
+local keymap = vim.keymap
+
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
+
+keymap.set("n", "<leader>l", "<cmd>Lazy<cr>", { desc = "Lazy" })
+keymap.set("n", "<leader>m", "<cmd>Mason<cr>", { desc = "Mason" })
+
+-- インクリメント/デクリメント
+keymap.set("n", "+", "<C-a>")
+keymap.set("n", "-", "<C-x>")
+
+-- 全選択
+keymap.set("n", "<C-a>", "gg<S-v>G")
+
+vim.api.nvim_create_autocmd("LspAttach", {
+    callback = function(ev)
+        -- 一括で変数名や関数名を変更
+        keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { buf = ev.buf, desc = "LSP Rename" })
+        -- クイックフィックス的な
+        keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, { buf = ev.buf, desc = "LSP Code Action" })
+    end,
+})
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,10 +1,14 @@
 return {
-	"nvim-telescope/telescope.nvim",
-	version = "*",
-	keys = {
-		{ "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Telescope find files" },
-		{ "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Telescope live grep" },
-		{ "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "Telescope buffers" },
-		{ "<leader>fh", "<cmd>Telescope help_tags<cr>", desc = "Telescope help tags" },
-	},
+    "nvim-telescope/telescope.nvim",
+    version = "*",
+    keys = {
+        { "<leader>ff", "<cmd>Telescope find_files<cr>",          desc = "Telescope find files" },
+        { "<leader>fg", "<cmd>Telescope live_grep<cr>",           desc = "Telescope live grep" },
+        { "<leader>fb", "<cmd>Telescope buffers<cr>",             desc = "Telescope buffers" },
+        { "<leader>fh", "<cmd>Telescope help_tags<cr>",           desc = "Telescope help tags" },
+        { "gd",         "<cmd>Telescope lsp_definitions<cr>",     desc = "Telescope go to definition",      mode = "n" },
+        { "gr",         "<cmd>Telescope lsp_references<cr>",      desc = "Telescope go to references",      mode = "n" },
+        { "gi",         "<cmd>Telescope lsp_implementations<cr>", desc = "Telescope go to implementations", mode = "n" },
+        { "gy",         "<cmd>Telescope lsp_type_definitions",    desc = "Telescope go to type definition", mode = "n" },
+    },
 }
