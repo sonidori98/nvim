9d58fe6b2ec44126641c9e166b8ef9a232b31ac1
sonidori <sonidori98@proton.me>
2026-06-16 22:35:49 +0900

    🔧 Tree sitter managerに移行

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -51,21 +51,3 @@ if uname.sysname == "Linux" then
         })
     end
 end
-
--- tree-sitter
-local ts_group = vim.api.nvim_create_augroup("treesitter_setup", { clear = true })
-
-vim.api.nvim_create_autocmd("FileType", {
-    group = ts_group,
-    pattern = { "cpp", "rust", "lua", "bash", "python", "c" },
-    callback = function(args)
-        local bufnr = args.buf
-        local ft = vim.bo[bufnr].filetype
-        local lang = vim.treesitter.language.get_lang(ft) or ft
-
-        local has_parser, _ = pcall(vim.treesitter.language.add, lang)
-        if has_parser then
-            vim.treesitter.start(bufnr, lang)
-        end
-    end,
-})
---
+++ lua/plugins/tree-sitter-manager.lua
@@ -0,0 +1,7 @@
+return {
+    "romus204/tree-sitter-manager.nvim",
+    event = { "BufReadPost", "BufNewFile" },
+    opts = {
+        auto_install = true,
+    },
+}
